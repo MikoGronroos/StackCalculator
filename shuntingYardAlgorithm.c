ffa7d76f749864bed86fa086784a7bf67b75c3e6
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-19 15:35:13 +0000

    Add while loop to go through a string

--- shuntingYardAlgorithm.c
+++ shuntingYardAlgorithm.c
@@ -1,16 +1,16 @@
 #include "stack.h"
 #include <stdio.h>
 
-int run(char* input){
-  printf("%s", input);
+int run(char input[]){
+  printf("%s\n------\n", input);
   stack* output = allocateNewStack();
   stack* operators = allocateNewStack();
   int index = 0;
-  char* character = &input[index];
-  while(*character != '\0'){
-    printf("%s", character);
+  char character = input[index];
+  while(character != '\0'){
+    printf("%c\n", character);
     index++;
-    character = &input[index];
+    character = input[index];
   }
-
+  return 0;
 }
