ae59963b8cb58ae3f55c50705d9835a8f284de64
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-19 15:16:08 +0000

    Chang stackcalculator to a

--- makefile
+++ makefile
@@ -2,4 +2,4 @@ CC=gcc
 CFLAGS=-I
 
 stackcalculator: main.c shuntingYardAlgorithm.c stack.c
-	$(CC) -o stackcalculator main.c shuntingYardAlgorithm.c stack.c
+	$(CC) -o a main.c shuntingYardAlgorithm.c stack.c
--- shuntingYardAlgorithm.c
+++ shuntingYardAlgorithm.c
@@ -1,7 +1,16 @@
 #include "stack.h"
+#include <stdio.h>
 
 int run(char* input){
-
+  printf("%s", input);
   stack* output = allocateNewStack();
+  stack* operators = allocateNewStack();
+  int index = 0;
+  char* character = &input[index];
+  while(*character != '\0'){
+    printf("%s", character);
+    index++;
+    character = &input[index];
+  }
 
 }
