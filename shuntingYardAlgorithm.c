45c59deb7acc7a3656c30c3bad17923d43e03e39
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-19 20:24:54 +0000

    Add first operator check

--- shuntingYardAlgorithm.c
+++ shuntingYardAlgorithm.c
@@ -3,45 +3,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int run(char input[]){
-  printf("%s\n------\n", input);
-  stack* output = allocateNewStack();
-  stack* operators = allocateNewStack();
-  int index = 0;
-  char character = input[index];
-  while(character != '\0'){
-    if(isdigit(character)){
-      char* outputChar = malloc(sizeof(char));
-      *outputChar = character;
-      addToStack(output, outputChar);
-    }else{
-      char* operatorChar = malloc(sizeof(char));
-      *operatorChar = character;
-      addToStack(operators, operatorChar);
-      if(operators->length > 1){
-        while(operators->stack[1] != '(' && (getPrecedence(operators->stack[1]) > getPrecedence(operators->stack[0]) || (getPrecedence(operators->stack[1]) == getPrecedence(operators->stack[0])  &&))){
-        
-        }
-      }
-    }
-    index++;
-    character = input[index];
-  }
-
-  for(int i = output->length - 1; i >= 0; i--){
-    printf("%s\n", output->stack[i].content);
-
-  }
-
-  printf("------\n");
-
-  for(int i = operators->length - 1; i >= 0; i--){
-    printf("%s\n", operators->stack[i].content);
-
-  }
-  return 0;
-}
-
 int getPrecedence(char operator){
   if(operator == '^'){
     return 4;
@@ -75,8 +36,47 @@ char* getAssociativity(char operator){
     return "left";
   }
   if(operator == '-'){
-    return "left"
+    return "left";
   }
   return "none";
 
+}
+
+int run(char input[]){
+  printf("%s\n------\n", input);
+  stack* output = allocateNewStack();
+  stack* operators = allocateNewStack();
+  int index = 0;
+  char character = input[index];
+  while(character != '\0'){
+    if(isdigit(character)){
+      char* outputChar = malloc(sizeof(char));
+      *outputChar = character;
+      addToStack(output, outputChar);
+    }else{
+      char* operatorChar = malloc(sizeof(char));
+      *operatorChar = character;
+      addToStack(operators, operatorChar);
+      if(operators->length > 1){
+        while(*operators->stack[1].content != '(' && (getPrecedence(*operators->stack[1].content) > getPrecedence(*operators->stack[0].content) || (getPrecedence(*operators->stack[1].content) == getPrecedence(*operators->stack[0].content) && getAssociativity(*operators->stack[0].content) == "left"))){
+        
+        }
+      }
+    }
+    index++;
+    character = input[index];
+  }
+
+  for(int i = output->length - 1; i >= 0; i--){
+    printf("%s\n", output->stack[i].content);
+
+  }
+
+  printf("------\n");
+
+  for(int i = operators->length - 1; i >= 0; i--){
+    printf("%s\n", operators->stack[i].content);
+
+  }
+  return 0;
 }
